bf2e06a925de0b9532416989f41d24eb8e63614f
KovDarlin <ki27.kovalenko.daryna@vtc.vn.ua>
2024-10-06 22:57:51 +0300

    ex 1 was done

--- main.cpp
+++ main.cpp
@@ -1,5 +1,47 @@
 #include <iostream>
+using namespace std;
+
+int sum(int x, int y) {
+	return x + y;
+}
+
+int difference(int x, int y) {
+	return x - y;
+}
+
+int product(int x, int y) {
+	if (x != 0 || y != 0)
+	{
+		return 0;
+	}
+	return x * y;
+}
+
+int fraction(int x, int y) {
+	if (x != 0 && y != 0)
+	{
+		return x / y;
+	}
+	cout << "O no problem with 0!!!!" << endl;
+	return 0;
+}
+
+int (*dii[])(int, int) = {sum, difference, product, fraction };
 
 int main() {
+	int a, b;
+	int operation;
+	cout << "Enter fitst number: ";
+	cin >> a;
+	cout << "Enter second number: ";
+	cin >> b;
+	cout << "Enter operation +(0), -(1), *(2), /(3): ";
+	cin >> operation;
+	do {
+		int res = dii[operation](a, b);
+		cout << res << endl;
+	} while (operation > 3);
+	
+
 
 }
